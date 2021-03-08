7221d827140acb0855fdd92a2a5170fe707ffc89
AlanACruz <cruz.alan@gmail.com>
2021-03-07 19:10:25 -0500

    updated Guava & changed Diatonic to simitonal

--- src/main/java/cruz/music/tone/scale/Diatonic.java
+++ src/main/java/cruz/music/tone/scale/Diatonic.java
@@ -1,6 +1,7 @@
 package cruz.music.tone.scale;
 
-import cruz.music.interval.Interval;
+import cruz.music.interval.Octave;
+import cruz.music.interval.Semitone;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,20 +18,20 @@ public class Diatonic extends Scale {
 	
 	private final int scaleLength = 7;
 	
-	private final int first = Interval.PERFECT_UNITY;
-	private final int second = Interval.MAJOR_SECOND;
-	private final int third = Interval.MAJOR_THIRD;
-	private final int fourth = Interval.PERFECT_FOURTH;
-	private final int fifth = Interval.PERFECT_FIFTH;
-	private final int sixth = Interval.MAJOR_SIXTH;
-	private final int seventh = Interval.MAJOR_SEVENTH;
+	private final int first = Semitone.ZERO;
+	private final int second = Semitone.TWO;
+	private final int third = Semitone.FOUR;
+	private final int fourth = Semitone.FIVE;
+	private final int fifth = Semitone.SEVEN;
+	private final int sixth = Semitone.NINE;
+	private final int seventh = Semitone.ELEVEN;
 	
 	/**
-	 * Return all tones in a scale.
+	 * Return all tones in an Ionian mode (I).
 	 * 
 	 * @return scale array
 	 */
-	public int[] getScale() {
+	public int[] getIonian() {
 
 		return new int[] {
 				getFirst(),
@@ -41,5 +42,161 @@ public class Diatonic extends Scale {
 				getSixth(),
 				getSeventh()
 		};
+	}
+	
+	/**
+	 * Return all tones in a Dorian mode (ii).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getDorian() {
+
+		int modeOffset = getSecond();
+		
+		return new int[] {
+				
+			getSecond() - modeOffset,
+			getThird() - modeOffset,
+			getFourth() - modeOffset,
+			getFifth() - modeOffset,
+			getSixth() - modeOffset,
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a Phrygian mode (iii).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getPhrygian() {
+
+		int modeOffset = getThird();
+		
+		return new int[] {
+				
+			getThird() - modeOffset,
+			getFourth() - modeOffset,
+			getFifth() - modeOffset,
+			getSixth() - modeOffset,
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE,
+			getSecond() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a Lydian mode (IV).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getLydian() {
+
+		int modeOffset = getFourth();
+		
+		return new int[] {
+				
+			getFourth() - modeOffset,
+			getFifth() - modeOffset,
+			getSixth() - modeOffset,
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE,
+			getSecond() - modeOffset + Octave.ONE,
+			getThird() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a Mixolydian mode (V).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getMixolydian() {
+
+		int modeOffset = getFifth();
+		
+		return new int[] {
+				
+			getFifth() - modeOffset,
+			getSixth() - modeOffset,
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE,
+			getSecond() - modeOffset + Octave.ONE,
+			getThird() - modeOffset + Octave.ONE,
+			getFourth() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a Aeolian mode (vi).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getAeolian() {
+
+		int modeOffset = getSixth();
+		
+		return new int[] {
+
+			getSixth() - modeOffset,
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE,
+			getSecond() - modeOffset + Octave.ONE,
+			getThird() - modeOffset + Octave.ONE,
+			getFourth() - modeOffset + Octave.ONE,
+			getFifth() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a Locrian mode (vii).
+	 * 
+	 * @return scale array
+	 */
+	public int[] getLocrian() {
+
+		int modeOffset = getSeventh();
+		
+		return new int[] {
+
+			getSeventh() - modeOffset,
+			getFirst() - modeOffset + Octave.ONE,
+			getSecond() - modeOffset + Octave.ONE,
+			getThird() - modeOffset + Octave.ONE,
+			getFourth() - modeOffset + Octave.ONE,
+			getFifth() - modeOffset + Octave.ONE,
+			getSixth() - modeOffset + Octave.ONE
+		};
+	}
+	
+	/**
+	 * Return all tones in a major.
+	 * 
+	 * @return scale array
+	 */
+	public int[] getMajor() {
+
+		return getIonian();
+	}
+	
+	/**
+	 * Return all tones in a minor scale.
+	 * 
+	 * @return scale array
+	 */
+	public int[] getMinor() {
+
+		return getAeolian();
+	}
+	
+	/**
+	 * Return all tones in a scale.
+	 * 
+	 * @return scale array
+	 */
+	public int[] getScale() {
+
+		return getMajor();
 	}
 }
