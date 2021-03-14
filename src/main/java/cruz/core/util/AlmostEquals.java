8581283623f7788406d8756f840379a86697b130
AlanACruz <cruz.alan@gmail.com>
2021-03-14 15:01:37 -0400

    moving useless util

--- src/main/java/cruz/core/util/AlmostEquals.java
+++ src/main/java/cruz/core/util/AlmostEquals.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-package cruz.util;
+package cruz.core.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
