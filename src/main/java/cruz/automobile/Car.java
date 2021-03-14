0bc8ea4f314fceea46c0be0dbd6425f74c7f7425
AlanACruz <cruz.alan@gmail.com>
2021-03-14 13:48:28 -0400

    Copyright

--- src/main/java/cruz/automobile/Car.java
+++ src/main/java/cruz/automobile/Car.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2019 Alan Cruz
+/*******************************************************************************
+ * Copyright (C) 2021 Alan Cruz
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -8,8 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -18,7 +18,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */
+ ******************************************************************************/
 package cruz.automobile;
 
 import org.springframework.stereotype.Component;
